4ebc343c1b77453ba9200da8971d5d32423af5a5
Ruslan <Ruslan@DESKTOP-2C7OVNH>
2024-09-21 21:01:22 +0300

    Add project files.

---
+++ BPD_Lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BPD_Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MD5 Hash of '': " + MD5.ComputeHash(""));
+            Console.WriteLine("MD5 Hash of 'a': " + MD5.ComputeHash("a"));
+            Console.WriteLine("MD5 Hash of 'abc': " + MD5.ComputeHash("abc"));
+            Console.WriteLine("MD5 Hash of 'message digest': " + MD5.ComputeHash("message digest"));
+        }
+
+        
+    }
+}
